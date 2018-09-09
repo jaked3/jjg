386c3a78027831293398d360d92958f02964a965
Jake Duff <42357602+jaked3@users.noreply.github.com>
2018-09-09 15:57:32 -0400

    initial

---
+++ jjg/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Jungle Juice Games </h1>
+        <h4> Coming Soon..... </h4>
+      </div>
+    );
+  }
+}
+
+export default App;
