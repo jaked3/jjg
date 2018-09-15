a30d9135e321d83e482db35997cd5bc31e6e30fe
Jake Duff <42357602+jaked3@users.noreply.github.com>
2018-09-15 13:14:21 -0400

    add home page

--- jjg/src/App.js
+++ jjg/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Route from 'react-router-dom/Route';
 import './App.css';
+import Home from './Components/Home';
 
 class App extends Component {
   render() {
     return (
+
       <div>
-        <h1> Jungle Juice Games </h1>
-        <h4> Coming Soon..... </h4>
+      <Router>
+        <Route path="/" exact strict render={ () => {
+            return (<Home />);
+          }
+          }/>
+          </Router>
       </div>
     );
   }
